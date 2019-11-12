e31819fc6979c10d97b88f2fae7141648a86903a
MuAdd <MuAdd@Zoho.com>
2019-11-12 00:18:42 -0500

    fixed mesh rotation logic

--- TopDownTwinStickController/Controller.gd
+++ TopDownTwinStickController/Controller.gd
@@ -9,7 +9,6 @@ export(float) var MovementSpeed = 15
 export(float) var Acceleration = 3
 export(float) var Deacceleration = 5
 export(float) var MaxJump = 19
-export(float) var MouseSensitivity = 2
 export(float) var RotationSpeed = 3
 export(float) var MaxZoom = 0.5
 export(float) var MinZoom = 1.5
@@ -22,7 +21,7 @@ var BulletPosition
 var RayCast 
 var InnerGimbal
 var Direction = Vector3()
-var Rotation = Vector2()
+var LastDirection = Vector3()
 var CameraRotation
 var gravity = -10
 var Accelerate = Acceleration
@@ -33,11 +32,14 @@ var Speed = Vector3()
 var CurrentVerticalSpeed = Vector3()
 var JumpAcceleration = 3
 var IsAirborne = false
-var Deadzone = 0.2
+var Joystick_Deadzone = 0.2
+var Mouse_Deadzone = 20
+
+enum ROTATION_INPUT{MOUSE, JOYSTICK, MOVE_DIR}
+
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
 	Player = get_node(PlayerPath)
 	Camera = get_node(CameraPath)
 	MeshInstance = get_node(MeshInstancePath)
@@ -47,36 +49,29 @@ func _ready():
 
 func _unhandled_input(event):
 	
-	var horizontal = Input.get_action_strength("look_right") - Input.get_action_strength("look_left")
-	var vertical = Input.get_action_strength("look_up") - Input.get_action_strength("look_back")
-	#Rotate Mesh with Mouse
-	if event is InputEventMouseMotion :
-		var rayLength = 100
-		var from = Camera.project_ray_origin(event.position)
-		var to = from + Camera.project_ray_normal(event.position)*rayLength
-		RayCast.translation = from
-		RayCast.cast_to = to
-		RayCast.force_raycast_update()
-		var collision_point = RayCast.get_collision_point()
-		MeshInstance.look_at(collision_point,Vector3.UP)
-		var rotationDegree = MeshInstance.get_rotation_degrees().y
-		MeshInstance.set_rotation_degrees(Vector3(90,rotationDegree + 180,0))
-	
 	#Rotation Mesh with Joystick
-	elif abs(horizontal) > Deadzone or abs(vertical) > Deadzone:
-		var rot = atan2(vertical,horizontal)*180/PI
-		rot += Camera.get_rotation_degrees().y 
-		rot += 90
-		MeshInstance.set_rotation_degrees(Vector3(90,rot,0))
-	
-	#Rotate Mesh from last Moved Direction
-	else:
-		var angle = atan2(Speed.x, Speed.z)*180/PI
-		var char_rot = MeshInstance.get_rotation_degrees()
-		char_rot.y = angle
-		MeshInstance.set_rotation_degrees(char_rot)
-	
-	
+	if event is InputEventJoypadMotion :
+		var horizontal = Input.get_action_strength("look_right") - Input.get_action_strength("look_left")
+		var vertical = Input.get_action_strength("look_up") - Input.get_action_strength("look_back")
+		if abs(horizontal) > Joystick_Deadzone or abs(vertical) > Joystick_Deadzone:
+			rotateMesh(Vector2(horizontal,vertical), ROTATION_INPUT.JOYSTICK)
+		else:
+			#Rotate Mesh from last Moved Direction (Left joystick)
+			rotateMesh(Speed,ROTATION_INPUT.MOVE_DIR)
+
+	#Rotate Mesh with Mouse Motion
+	elif event is InputEventMouseMotion:
+		if magnitude(event.get_speed()) > Mouse_Deadzone: # or event is InputEventMouseButton:
+			rotateMesh(event, ROTATION_INPUT.MOUSE)
+			
+		else:
+			#Rotate Mesh from last Moved Direction (WASD Key presses)
+			rotateMesh(Speed, ROTATION_INPUT.MOVE_DIR)
+
+	#Rotate Mesh with Mouse Button Left
+	elif event is InputEventMouseButton and event.get_button_index() == BUTTON_LEFT:
+		rotateMesh(event, ROTATION_INPUT.MOUSE)
+
 	#Zoom
 	if event is InputEventMouseButton:
 		match event.button_index:
@@ -92,8 +87,42 @@ func _unhandled_input(event):
 			KEY_ESCAPE:
 				get_tree().quit()
 
-	
-	
+func rotateMesh(event_data, input_method):
+	match input_method:
+		ROTATION_INPUT.MOUSE:
+			#event_data is mouse position in viewport
+			var rayLength = 100
+			var from = Camera.project_ray_origin(event_data.position)
+			var to = from + Camera.project_ray_normal(event_data.position)*rayLength
+			RayCast.translation = from
+			RayCast.cast_to = to
+			RayCast.force_raycast_update()
+			var collision_point = RayCast.get_collision_point()
+			MeshInstance.look_at(collision_point,Vector3.UP)
+			var rotationDegree = MeshInstance.get_rotation_degrees().y
+			MeshInstance.set_rotation_degrees(Vector3(90,rotationDegree + 180,0))
+		ROTATION_INPUT.JOYSTICK:
+			#event_data is right joystick axis strength
+			var rot = atan2(event_data.y,event_data.x)*180/PI
+			rot += InnerGimbal.get_rotation_degrees().y 
+			rot += 90
+			MeshInstance.set_rotation_degrees(Vector3(90,rot,0))
+		ROTATION_INPUT.MOVE_DIR:
+			#event_data is directional vector to rotate player
+			#Check if Player is moving and new movement is different than last direction
+			if magnitude(event_data) > 0 and LastDirection.dot(event_data.normalized()) != 0:
+				#Rotate in Direction of Movement
+				var angle = atan2(event_data.x, event_data.z)
+				var char_rot = MeshInstance.get_rotation()
+				var rot_y = angle - char_rot.y  
+				MeshInstance.rotate_y(rot_y)
+
+#Helper math function
+func magnitude(vector):
+	if typeof(vector) == typeof(Vector2()):
+		return sqrt(vector.x*vector.x + vector.y*vector.y)
+	elif typeof(vector) == typeof(Vector3()):
+		return sqrt(vector.x*vector.x + vector.z*vector.z)
 
 func _process(delta):
 	#Shoot
@@ -127,13 +156,14 @@ func _physics_process(delta):
 	if(Input.is_action_pressed("move_right")):
 		Direction += CameraTransform.basis[0]
 	Direction.y = 0
+	LastDirection = Direction.normalized()
 	var MaxSpeed = MovementSpeed * Direction.normalized()
 	Accelerate = Deacceleration
 	if(Direction.dot(Speed) > 0):
 		Accelerate = Acceleration
 	Direction = Vector3.ZERO
 	Speed = Speed.linear_interpolate(MaxSpeed, delta * Accelerate)
-	#Movement = Player.transform.basis * (Speed)
+	Movement = Player.transform.basis * (Speed)
 	Movement = Speed
 	CurrentVerticalSpeed.y += gravity * delta * JumpAcceleration
 	Movement += CurrentVerticalSpeed
